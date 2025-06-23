508a5e1d532a2e9e5e53653791c49a6af7bdd1f7
code4bau <merloauti@gmail.com>
2025-06-23 13:07:50 -0300

    agregamos pdfs

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/bautista/Documentos/folp-web/venv
+set -gx VIRTUAL_ENV /home/bautista/ProyectosFolp/folp-web/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
